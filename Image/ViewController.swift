33d57b43c6764eae1f8dcf3e4c04df58568d357b
Matt Deuschle <mdeuschle@gmail.com>
2018-10-06 17:30:08 -0700

    add view

---
+++ Image/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Image
+//
+//  Created by Matt Deuschle on 10/6/18.
+//  Copyright © 2018 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView
+
+    
+}
+
